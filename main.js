3a1fe7102b70cc645d6858b44c75c5b822aa5f93
boyerk <35824068+boyerk@users.noreply.github.com>
2018-11-28 16:17:56 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+var slideIndex = 1;
+var slides = document.getElementsByClassName("mySlides");
+showSlides(slideIndex);
+setInterval(function () {
+    loop()
+}, 7000);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    console.log("showSlides");
+}
+
+function loop() {
+    slideIndex++;
+    console.log("Loop")
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    // setTimeout(showSlides, 1000);
+    showSlides(slideIndex);
+}
